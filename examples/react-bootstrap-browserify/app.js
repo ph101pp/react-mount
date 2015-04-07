922dfbd0e44e4ea0a47af03cd26eef1176aac377
Philipp C. Adrian <hello@philippadrian.com>
2015-04-06 22:24:06 -0400

    react-bootstrap-browserify example

---
+++ examples/react-bootstrap-browserify/app.js
@@ -0,0 +1,6 @@
+var mount = require("../../react-mount.js");
+var bootstrap = require("react-bootstrap");
+
+mount(bootstrap, {
+	attributes: ["bsStyle", "bsSize", "bsClass"]
+});
---
+++ examples/react-bootstrap-browserify/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp         = require('gulp');
+var rename       = require('gulp-rename');
+var uglify       = require('gulp-uglify');
+var _browserify  = require('browserify');
+var transform    = require('vinyl-transform');
+
+///////////////////////////////////////////////////////////////////////////////
+
+gulp.task("default", ["scripts"]);
+gulp.task("scripts", scripts);
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+function browserify(){
+  return transform(function(filename){
+    var b =  _browserify(filename);
+    return b.bundle();
+  });
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function scripts(){
+  gulp.src("app.js")
+    .pipe(browserify())
+    // .pipe(uglify())
+    .pipe(rename("react-bootstrap-mounted.js"))
+    .pipe(gulp.dest("./"));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
